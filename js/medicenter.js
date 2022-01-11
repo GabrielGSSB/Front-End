2b10011278c623835f1bc7b8ede5f15b7248ee1e
GabrielGSSB <97188591+GabrielGSSB@users.noreply.github.com>
2022-01-11 00:01:04 -0300

    Add files via upload

---
+++ js/medicenter.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+        window.onload = function(){
+            document.querySelector(".menumobile").addEventListener("click", function(){
+                if(document.querySelector(".menu nav ul").style.display == 'flex') {
+                    document.querySelector(".menu nav ul").style.display = 'none';
+                } else {
+                    document.querySelector(".menu nav ul").style.display = 'flex';
+                }
+            });
+        };
+        </script>
